1c37db525cc7fdf4f7d434049ed15e03d018f3d7
Marcelo de Moura Cavalcanti Junior <marcelo.cavalcanti@mv.com.br>
2023-10-25 09:07:53 -0300

    commit update parking

---
+++ parking-control/src/main/java/com/br/parkingcontrol/controller/PArkingController.java
@@ -0,0 +1,4 @@
+package com.br.parkingcontrol.controller;
+
+public class PArkingController {
+}
--- parking-control/src/main/java/com/br/parkingcontrol/service/ParkingService.java
+++ parking-control/src/main/java/com/br/parkingcontrol/service/ParkingService.java
@@ -1,4 +1,14 @@
 package com.br.parkingcontrol.service;
 
+import com.br.parkingcontrol.repositories.ParkingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ParkingService {
+   final ParkingRepository parkingRepository;
+
+    public ParkingService(ParkingRepository parkingRepository) {
+        this.parkingRepository = parkingRepository;
+    }
 }
