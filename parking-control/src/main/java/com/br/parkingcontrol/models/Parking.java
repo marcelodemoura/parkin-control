4350c5d624a7a151d49501e16ba58c2202ab86e7
Marcelo de Moura Cavalcanti Junior <marcelo.cavalcanti@mv.com.br>
2023-10-25 09:07:53 -0300

    commit update parking

---
+++ parking-control/src/main/java/com/br/parkingcontrol/models/Parking.java
@@ -0,0 +1,40 @@
+package com.br.parkingcontrol.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = " tb_parking)")
+public class Parking implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, unique = true, length = 10)
+    private String parkingNumber;
+    @Column(nullable = false, unique = true, length = 7)
+    private String licensePlateCar;
+    @Column(nullable = false, unique = true, length = 70)
+    private String brandCar;
+    @Column(nullable = false, unique = true, length = 70)
+    private String modelCar;
+    @Column(nullable = false, unique = true, length = 70)
+    private String collorCar;
+    @Column(nullable = false)
+    private LocalDateTime registrationDate;
+    @Column(nullable = false, length = 130)
+    private String responsibleName;
+    @Column(nullable = false, length = 20)
+    private String apartamnet;
+    @Column(nullable = false, length = 20)
+    private String block;
+
+}
