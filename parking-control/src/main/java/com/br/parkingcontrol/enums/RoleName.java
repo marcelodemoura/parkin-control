dbbdd437c0e4a01ca9301f726efd38427e08a87d
Marcelo de Moura Cavalcanti Junior <marcelo.cavalcanti@mv.com.br>
2023-11-07 14:57:54 -0300

    commit criacao UserRepository, RoleName

---
+++ parking-control/src/main/java/com/br/parkingcontrol/enums/RoleName.java
@@ -0,0 +1,8 @@
+package com.br.parkingcontrol.enums;
+
+public enum RoleName {
+
+    Role_Admin,
+    Role_User
+
+}
---
+++ parking-control/src/main/java/com/br/parkingcontrol/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package com.br.parkingcontrol.repositories;
+
+import com.br.parkingcontrol.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+
+    Optional<UserModel>findByUsername(String username);
+
+
+
+}
