9c1c9dfec4650bcd7776084e4849a970e64f0da7
Marcelo de Moura Cavalcanti Junior <marcelo.cavalcanti@mv.com.br>
2023-11-07 14:53:08 -0300

    commit update class security, parking controller

--- parking-control/src/main/java/com/br/parkingcontrol/config/security/WebSecurityConfig.java
+++ parking-control/src/main/java/com/br/parkingcontrol/config/security/WebSecurityConfig.java
@@ -1,12 +1,43 @@
 package com.br.parkingcontrol.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
+
 public class WebSecurityConfig {
 
+    final UserDetailsServiceImpl userDetailsService;
 
+    public WebSecurityConfig(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(x ->
+                        x
+                                .requestMatchers(HttpMethod.GET, "/parking/**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/parking/**").hasRole("User")
+                                .requestMatchers(HttpMethod.DELETE, "/parking/**").hasRole("Admin")
+                                .anyRequest().authenticated());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
 
 
--- parking-control/src/main/java/com/br/parkingcontrol/controller/ParkingController.java
+++ parking-control/src/main/java/com/br/parkingcontrol/controller/ParkingController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -27,6 +28,7 @@ public class ParkingController {
         this.parkingService = parkingService;
     }
 
+    @PreAuthorize("hasRole('Role_Admin')")
     @PostMapping
     public ResponseEntity<Object> saveParking(@RequestBody @Valid ParkingDto parkingDto) {
         if (parkingService.existsByLicensePlateCar(parkingDto.getLicensePlateCar())) {
@@ -45,11 +47,14 @@ public class ParkingController {
         return ResponseEntity.status(HttpStatus.CREATED).body(parkingService.save(parking));
     }
 
+
+    @PreAuthorize("hasRole('Role_User','Role_Admin')")
     @GetMapping("/todos")
     public ResponseEntity<List<Parking>> getAllParking() {
         return ResponseEntity.status(HttpStatus.OK).body(parkingService.findAll());
     }
 
+    @PreAuthorize("hasRole('Role_User','Role_Admin')")
     @GetMapping("/{id}")
     public ResponseEntity<Object> getIdParking(@PathVariable(value = "id") UUID id) {
         Optional<Parking> parkingOptional = parkingService.findById(id);
@@ -59,6 +64,7 @@ public class ParkingController {
         return ResponseEntity.status(HttpStatus.OK).body(parkingOptional.get());
     }
 
+    @PreAuthorize("hasRole('Role_Admin')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteParking(@PathVariable(value = "id") UUID id) {
         Optional<Parking> parkingOptional = parkingService.findById(id);
@@ -69,11 +75,13 @@ public class ParkingController {
         return ResponseEntity.status(HttpStatus.OK).body("Successfully");
 
     }
+
+    @PreAuthorize("hasRole('Role_Admin')")
     @PutMapping("/{id}")
-    public ResponseEntity<Object>updateParking(@PathVariable(value = "id")UUID id,
-                                               @RequestBody @Valid ParkingDto parkingDto){
-        Optional<Parking>parkingOptional = parkingService.findById(id);
-        if (!parkingOptional.isPresent()){
+    public ResponseEntity<Object> updateParking(@PathVariable(value = "id") UUID id,
+                                                @RequestBody @Valid ParkingDto parkingDto) {
+        Optional<Parking> parkingOptional = parkingService.findById(id);
+        if (!parkingOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(("Parking not found"));
         }
         var parking = new Parking();
